56091fafe1fdf4c6ae0d9b22432e96dade9d91d8
vitalii-yenko <vitalii.yenko@ringteam.com>
2025-03-04 12:49:03 +0200

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.ukraine4ever.justcalendar"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
